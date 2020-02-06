3468d8759c45b5c1e3363cdcca55f13d67c805bd
joeymedina <joeymedina@CIS-RDS2>
2020-02-05 18:03:01 -0600

    everything

--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace MonoGameWindowsStarter
 {
@@ -13,10 +14,17 @@ namespace MonoGameWindowsStarter
         SpriteBatch spriteBatch;
         Texture2D man;
         Texture2D texture;
+        Texture2D badman;
+        Texture2D win;
+        Texture2D lose;
         Rectangle manRect;
-        int manSpeedUpDown = 0;
-        int manSpeedLeftRight = 0;
+        Rectangle badmanRect;
+        Rectangle finishRect;
         Texture2D finish;
+        Random ran;
+        bool won;
+        bool lost;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -34,6 +42,7 @@ namespace MonoGameWindowsStarter
             // TODO: Add your initialization logic here
             graphics.PreferredBackBufferWidth = 1042;
             graphics.PreferredBackBufferHeight = 768;
+            ran = new Random();
             graphics.ApplyChanges();
 
             base.Initialize();
@@ -47,14 +56,25 @@ namespace MonoGameWindowsStarter
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            man = Content.Load<Texture2D>("man2");
+            //man = Content.Load<Texture2D>("man2");
             texture = Content.Load<Texture2D>("pixel");
             finish = Content.Load<Texture2D>("finish");
+            badman = Content.Load<Texture2D>("badman");
+            win = Content.Load<Texture2D>("win");
+            lose = Content.Load<Texture2D>("lose");
             manRect.X = 50;
             manRect.Y = 400;
             manRect.Width = 75;
             manRect.Height = 75;
 
+            badmanRect.X = 800;
+            badmanRect.Y = ran.Next(400, 535);
+            badmanRect.Width = 55;
+            badmanRect.Height = 55;
+
+            finishRect = new Rectangle(925, 400, 100, 200);
+            won = false;
+            lost = false;
             // TODO: use this.Content to load your game content here
         }
 
@@ -82,34 +102,36 @@ namespace MonoGameWindowsStarter
 
             if (keyboardState.IsKeyDown(Keys.Up))
             {
-                manSpeedUpDown -= 1;
+                manRect.Y -= 5;
             }
             if (keyboardState.IsKeyDown(Keys.Down))
             {
-                manSpeedUpDown += 1;
+                manRect.Y += 5;
             }
 
             if (keyboardState.IsKeyDown(Keys.Left))
             {
-                manSpeedLeftRight -= 1;
+                manRect.X -= 5;
             }
             if (keyboardState.IsKeyDown(Keys.Right))
             {
-                manSpeedLeftRight += 1;
+                manRect.X += 5;
             }
             
-manRect.Y += manSpeedUpDown;
-            manRect.X += manSpeedLeftRight;
+    
+            //top
+            if (manRect.Y < 345)
+            {
+                manRect.Y = 345;
+            }
 
-            
-            if (manRect.Y < 0)
+            if (manRect.Y > 600 - manRect.Height)
             {
-                manRect.Y = 0;
+                manRect.Y = 600 - manRect.Height;
             }
-            if (manRect.Y > GraphicsDevice.Viewport.Height - manRect.Height)
-            {
-                manRect.Y = GraphicsDevice.Viewport.Height - manRect.Height;
-            }
+
+ 
+
 
             if (manRect.X < 0)
             {
@@ -120,7 +142,30 @@ manRect.Y += manSpeedUpDown;
             {
                 manRect.X = GraphicsDevice.Viewport.Width - manRect.Width;
             }
-            
+
+            if(!lost) badmanRect.X -= 5;
+            if (badmanRect.X < 0)
+            {
+                badmanRect.X = 825;
+                badmanRect.Y = ran.Next(400, 535);
+            }
+
+            if (manRect.Intersects(badmanRect))
+            {
+                lost = true;
+                badmanRect.X = 825;
+                badmanRect.X -= 5;
+                manRect.X = 50;
+            }
+
+            if (manRect.Intersects(finishRect))
+            {
+                won = true;
+                badmanRect.X = 825;
+                badmanRect.X -= 5;
+                manRect.X = 50;
+            }
+            //if(manRect.Intersects())
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -132,11 +177,25 @@ manRect.Y += manSpeedUpDown;
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.AliceBlue);
             spriteBatch.Begin();
+
             spriteBatch.Draw(man, manRect, Color.White);
-            spriteBatch.Draw(texture, new Rectangle(0, 600, 1042, 10), Color.Black);
-            spriteBatch.Draw(texture, new Rectangle(925, 400, 100, 200), Color.Yellow);
+            spriteBatch.Draw(badman, badmanRect, Color.White);
+
+            spriteBatch.Draw(texture, new Rectangle(0, 0, 1042, 345), Color.Black);
+            spriteBatch.Draw(texture, new Rectangle(0, 600, 1042, 175), Color.Black);
+            spriteBatch.Draw(finish, finishRect, Color.Yellow);
+
+            if (won)
+            {
+                spriteBatch.Draw(win, new Rectangle(525, 384, 200, 100), Color.MediumPurple);
+            }
+            if (lost)
+            {
+                spriteBatch.Draw(lose, new Rectangle(525, 384, 200, 100), Color.MediumVioletRed);
+            }
+
             spriteBatch.End();
             // TODO: Add your drawing code here
 
